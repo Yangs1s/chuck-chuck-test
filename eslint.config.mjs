8a6f51643d6425a96bb316d51acd56d7a4546055
YangS1s <sungjinahim@gmail.com>
2026-01-15 14:18:05 +0900

    feat: husky 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,19 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      // 사용하지 않는 변수 금지 (코드 지저분해지는 것 방지)
+      "@typescript-eslint/no-unused-vars": ["error"],
+      /* 2. 디버깅 코드 프로덕션 유입 방지 */
+      // console.log가 배포 코드에 들어가는 건 아마추어 같음 (warn 또는 error)
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      /* 3. React/Next.js 관련 */
+      // useEffect 의존성 배열 누락 방지 (버그의 주범)
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
