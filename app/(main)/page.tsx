7e435f8ccbab34f762cf930c053ea3bdbad186ba
YangS1s <sungjinahim@gmail.com>
2026-01-16 11:33:51 +0900

    fix: type 수정

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,14 +2,16 @@ import { ProductContainer } from "./components/ProductContainer";
 import { fetchApi } from "@/app/lib/fetchApi";
 import { ProductProps } from "@/app/types";
 
-interface ApiResponse {
-  content: ProductProps[];
-}
-
+type ApiResponse<T> = {
+  content: T;
+};
 const getData = async () => {
-  const data = await fetchApi<ApiResponse>("https://api.zeri.pics/", {
-    cache: "no-store",
-  });
+  const data = await fetchApi<ApiResponse<ProductProps[]>>(
+    "https://api.zeri.pics/",
+    {
+      cache: "no-store",
+    },
+  );
   return Array.isArray(data?.content) ? data!.content : [];
 };
 
